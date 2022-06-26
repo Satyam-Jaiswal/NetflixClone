dbf4a904fda282127292526b27e1da5ad480946d
Satyam Jaiswal <satyam.jaiswal26@gmail.com>
2022-06-26 17:48:14 +0530

    Updates

---
+++ precache-manifest.1250f7b9d2f58af98917ccbbec1b5aef.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "caf8d11fb324dac44077bf99beff76c2",
+    "url": "https://github.com/Satyam-Jaiswal/NetflixClone/index.html"
+  },
+  {
+    "revision": "cf232d347555f43cd0c2",
+    "url": "https://github.com/Satyam-Jaiswal/NetflixClone/static/css/main.9730889b.chunk.css"
+  },
+  {
+    "revision": "8d4897a6718d183dd703",
+    "url": "https://github.com/Satyam-Jaiswal/NetflixClone/static/js/2.f96ca391.chunk.js"
+  },
+  {
+    "revision": "cf232d347555f43cd0c2",
+    "url": "https://github.com/Satyam-Jaiswal/NetflixClone/static/js/main.7642d969.chunk.js"
+  },
+  {
+    "revision": "792b086d78f2fb29877e",
+    "url": "https://github.com/Satyam-Jaiswal/NetflixClone/static/js/runtime~main.6583eca9.js"
+  },
+  {
+    "revision": "149a098f51438682ddb64f6e0c0c709b",
+    "url": "https://github.com/Satyam-Jaiswal/NetflixClone/static/media/landingPage.149a098f.jpg"
+  },
+  {
+    "revision": "8562b6565f5ae1db5e4af40d85b4ed2d",
+    "url": "https://github.com/Satyam-Jaiswal/NetflixClone/static/media/netflix.8562b656.png"
+  },
+  {
+    "revision": "991556c2526eb97f8e745b79363c8661",
+    "url": "https://github.com/Satyam-Jaiswal/NetflixClone/static/media/normal.991556c2.jpg"
+  },
+  {
+    "revision": "c2b89779dd6b85078fc0cc2f3a9aaf28",
+    "url": "https://github.com/Satyam-Jaiswal/NetflixClone/static/media/profile.c2b89779.jpg"
+  }
+]);
